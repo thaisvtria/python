36e29c3097105fd9934056fe9a8cf9e6c82c640d
Thaís Vitória ⭒๋࣭⭑ <vitoria24thais@gmail.com>
2024-08-29 11:48:37 -0300

    Add files via upload

---
+++ aula.03.py
@@ -0,0 +1,16 @@
+## Variáveis em Python 
+## Não a necessidade de declará-las
+## Basta criar no meio do algoritmo com  a atribuição
+
+teste = "Thaís"
+print(teste) # Imprime a string "Thaís"
+print(type (teste))
+teste = 10
+print(teste) # Imprime o número 10
+print(type(teste)) # Imprime o tipo da variável, que é int
+teste = 1.74
+print(teste)
+print(type(teste)) # Imprime o tipo da variável, que é float
+teste = True
+print(teste) # Imprime o valor de True
+print(type(teste)) # Imprime o tipo da variável, que é bool
