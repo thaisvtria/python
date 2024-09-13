cdd7d570907f7f4afd42b531ec1fae13e44335a5
Thaís Vitória ⭒๋࣭⭑ <vitoria24thais@gmail.com>
2024-09-13 10:06:43 -0300

    nome - idade.py

---
+++ nome - idade.py
@@ -0,0 +1,15 @@
+# atribuição
+nome = "Thaís"
+idade = 16
+altura = 1.68 
+
+# exibir mensagens na tela 
+print ("nome: ", nome, "idade:" , idade, "altura: ", altura) 
+# formatação  com f-string
+print(f"nome: {nome}, idade: {idade}, altura: {altura}")  
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"nome: {nome} idade: {idade} altura {altura}")
