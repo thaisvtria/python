c7827c95f5ab510aee2fd16997a5d0836642ef3a
Thaís Vitória ⭒๋࣭⭑ <vitoria24thais@gmail.com>
2024-09-13 09:15:36 -0300

    aula6.py

---
+++ aula6.py
@@ -0,0 +1,22 @@
+# Operadores ariteticos
+# Soma
+soma = 10 + 5 
+print(soma)
+# Subtração
+subtracao = 10 - 5
+print(subtracao)
+# Multiplicação
+multiplicacao = 10 * 5
+print(multiplicacao)
+# Divisão
+divisao = 10 / 5
+print(divisao)
+# Divisão inteira
+divisao_inteira = 10 // 5
+print(divisao_inteira)
+# Exponenciação
+exponenciacao = 10 ** 2
+print(exponenciacao)
+# Módulo / resto da divisão 
+módulo = 10 % 5
+print(módulo)
