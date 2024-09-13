18554e38bf7779e6974cf6ef3fc1528bf6ba09d4
Thaís Vitória ⭒๋࣭⭑ <vitoria24thais@gmail.com>
2024-09-13 09:16:19 -0300

    aula5.py

---
+++ aula5.py
@@ -0,0 +1,13 @@
+# Tipos de dados em python
+# Inteiros
+inteiro = 10
+print(inteiro)
+# Floats
+float = 10.5
+print(float)
+# Strings
+string = "Olá, mundo!"
+print(string)
+# Booleannos
+booleano = True
+print(booleano)
