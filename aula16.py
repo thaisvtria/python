c1d6dc0c9801dab2ca86c1ec6dc6296ca8318097
Thaís Vitória ⭒๋࣭⭑ <vitoria24thais@gmail.com>
2024-09-26 12:01:56 -0300

    Add files via upload

---
+++ aula16.py
@@ -0,0 +1,7 @@
+def somar(a , b):
+    return a + b
+print("Algoritmo de Soma")
+valor1 = int(input("Digite o valor 1: "))
+valor2 = int(input("Digite o valor 2: "))
+resultado = somar(valor1, valor2)
+print(f"O resultado da soma: {resultado}")
